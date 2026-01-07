1c0fe60d9f87ac8a9224966f7c114cf9badef728
SofiiaHlushchak <glushchak02@gmail.com>
2026-01-07 13:13:30 +0100

    feat: add validation to create project

--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -1,46 +1,75 @@
 import { useRef } from "react";
 import Input from "../Input/Input";
+import Modal from "../Modal/Modal";
 
-export default function NewProject({ onAddProject }) {
+export default function NewProject({ onAddProject, onCancelProject }) {
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
+
+    const modal = useRef();
 
     function handleSave() {
         const enteredTitle = title.current.value;
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
-        onAddProject({
-            title: enteredTitle,
-            description: enteredDescription,
-            dueDate: enteredDueDate,
-        });
+        if (
+            !enteredTitle.trim() ||
+            !enteredDescription.trim() ||
+            !enteredDueDate.trim()
+        ) {
+            modal.current.open();
+        } else {
+            onAddProject({
+                title: enteredTitle,
+                description: enteredDescription,
+                dueDate: enteredDueDate,
+            });
+        }
     }
+
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">
-                        Cancel
-                    </button>
-                </li>
-                <li>
-                    <button
-                        onClick={handleSave}
-                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                    >
-                        Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input type="text" label="Title" ref={title} />
+        <>
+            <Modal ref={modal} buttonCaption="Close">
+                <h2 className="text-xl font-bold text-stone-700 my-4">
+                    Invalid Input
+                </h2>
+                <p className="textstone-600 mb-4">
+                    Oops... looks like you forgot to enter a value.
+                </p>
+                <p className="textstone-600 mb-4">
+                    Please make sure you provide a valid value for every input
+                    field.
+                </p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button
+                            className="text-stone-800 hover:text-stone-950"
+                            onClick={onCancelProject}
+                        >
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            onClick={handleSave}
+                            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                        >
+                            Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" label="Title" ref={title} />
 
-                <Input label="Description" textarea ref={description} />
+                    <Input label="Description" textarea ref={description} />
 
-                <Input type="date" label="Due Date" ref={dueDate} />
+                    <Input type="date" label="Due Date" ref={dueDate} />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
